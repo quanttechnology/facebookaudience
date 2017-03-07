44e694558f2040b1ee51c5ce8ae5286d2c3ca0db
kraisorn-veda <kraisorn@vedanetwork.com>
2017-03-07 21:48:27 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -7,10 +7,10 @@ var formData = {
     name: 'My new CA',
     subtype: 'CUSTOM',
     description: 'People who bought from my website',
-    access_token: 'EAASyCucqJ8IBALAStRpmGSRgTqL46S2Lpd5RMaLVArgJlKhZC3dJkxjCKtxR5O811usGxDrkA5l0M4PuBkUpgiDL5pRZAYZC6qZCOCY0o3eIyPMLlfqkKPK9Fk5Bvf9fb12MCEQAoGDTUZAYvmPeFQklSygSw3oRQsrZA7Ej5ZAsAZDZD'
+    access_token: '<ACCESS_TOKEN>'
 };
 
-request.post({url:'https://graph.facebook.com/v2.8/act_1384318948520536/customaudiences', formData: formData}, function optionalCallback(err, res, body) {
+request.post({url:'https://graph.facebook.com/v2.8/act_<AD_ACCOUNT_ID>/customaudiences', formData: formData}, function optionalCallback(err, res, body) {
     if (err) {
         return console.error('upload failed:', err);
     }
